c6b9be421f593d2c886dd6db683bbab230e70d90
marcelo.mesquita <marcelo.mesquita@codex.com.br>
2023-11-11 09:45:04 -0300

    feat: updated stuff

--- 2015/day01/main.go
+++ 2015/day01/main.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+
+	"github.com/alexchao26/advent-of-code-go/util"
+)
+
 func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	var ans int
+	if part == 1 {
+		ans = part1(util.ReadFile("./input.txt"))
+	} else {
+		ans = part2(util.ReadFile("./input.txt"))
+	}
+	fmt.Println("Output:", ans)
+}
+
+func part1(input string) int {
+	return 1
+}
+
+func part2(input string) int {
+	return 2
 }
